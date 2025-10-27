8dd62fc253ebb34b6e57e997657dbba07f9d4599
Mauro Sebastian Arias <maurosebastianarias@gmail.com>
2025-10-27 02:52:43 -0300

    Creacion de conexion a base de datos para CategoriaCurso

---
+++ Negocio/CategoriaCursoNegocio.cs
@@ -0,0 +1,49 @@
+﻿using Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Negocio
+{
+    public class CategoriaCursoNegocio
+    {
+
+        public List<CategoriaCurso> listar()
+        {
+            List<CategoriaCurso> lista = new List<CategoriaCurso>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                //seteo la consulta
+                datos.setearConsulta("SELECT IDCategoria,NombreCategoria FROM CategoriaCurso;");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    CategoriaCurso aux = new CategoriaCurso();
+                    aux.Id = (int)datos.Lector["IDCategoria"];
+                    aux.Nombre = (string)datos.Lector["NombreCategoria"];
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+        }
+    }
+
+}

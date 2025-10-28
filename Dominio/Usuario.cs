674f9b4fdd6909a82980d076d7a057abe892fd30
SebastianDte <sebateunmate@outlook.com>
2025-10-28 16:56:45 -0300

    Arreglo en el Header y Footer

---
+++ Dominio/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Usuario
+    {
+        public int UsuarioID { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; } 
+        public string Rol { get; set; } 
+        public string EstadoCuenta { get; set; } 
+        public bool EstaActivo { get; set; } 
+        public string UrlFotoPerfil { get; set; } 
+
+        public Usuario() { }
+        public bool EsAdmin()
+        {
+            return this.Rol == "Admin";
+        }
+    }
+}

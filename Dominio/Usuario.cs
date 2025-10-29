ef68c81ddfacabb6bfd0a28427dad7319adcbe5d
SebastianDte <sebateunmate@outlook.com>
2025-10-29 13:19:42 -0300

    Refactorización clase usuario

--- Dominio/Usuario.cs
+++ Dominio/Usuario.cs
@@ -10,18 +10,19 @@ namespace Dominio
     {
         public int UsuarioID { get; set; }
         public string Email { get; set; }
-
         public string PasswordHash { get; set; } 
-        public string Rol { get; set; } 
-        public string EstadoCuenta { get; set; } 
-        public bool EstaActivo { get; set; } 
+        public bool EstaActivo { get; set; } // Baja lógica
         public string UrlFotoPerfil { get; set; } 
 
+        
+        public int RolID { get; set; }
+        public int EstadoCuentaID { get; set; }
+
+        // --- Propiedades de Navegación (Opcionales) ---
+        public Rol Rol { get; set; } //ADMIN - PARTICIPANTE
+        public EstadoCuenta EstadoCuenta { get; set; } //Pendiente - Activo
+
         public Usuario() { }
-        public bool EsAdmin()
-        {
-            return this.Rol == "Admin";
-        }
 
     }
 }

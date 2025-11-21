6eb164461a81f50cb545e0dd855d1fd74b7497e7
Mauro Sebastian Arias <maurosebastianarias@gmail.com>
2025-11-21 00:55:44 -0300

    Creacion de PagoDatos y PagoNegocio

--- Negocio/InscripcionNegocio.cs
+++ Negocio/InscripcionNegocio.cs
@@ -10,55 +10,51 @@ namespace Negocio
 {
     public class InscripcionNegocio
     {
-        public InscripcionDatos datos { get; set;}
-
         public void CrearInscripcion(int idUsuario, int idCurso, DateTime fechaExpiracion)
         {   
-            try
-            {
-                Inscripcion nuevaInscripcion = new Inscripcion();
-               
-                //Validar si el curso esta publicado
-                nuevaInscripcion.CursoID = idCurso;
-                
-                //Validar si el Usuario esta ok
-                nuevaInscripcion.UsuarioID = idUsuario;
-                
-                nuevaInscripcion.FechaInscripcion = DateTime.Today;
-                nuevaInscripcion.FechaExpiracion = fechaExpiracion;
-                
-                //Los argumentos los obtengo previemente en el evento aceptar:
-                //idUsuario = int.Parse( ddlCurso.SelectedValue )
-                //idCurso = int.Parse( ddlCurso.SelectedValue )
-                //fechaExpiracion=> calendario = nuevaInscripcion.CursoID;
+ 
+            Inscripcion nuevaInscripcion = new Inscripcion();
+            InscripcionDatos datos = new InscripcionDatos();
 
-                datos.AltaInscripcion(nuevaInscripcion);
+            //Validar si el curso esta publicado
+            nuevaInscripcion.CursoID = idCurso;
+                
+            //Validar si el Usuario esta ok
+            nuevaInscripcion.UsuarioID = idUsuario;
+                
+            nuevaInscripcion.FechaInscripcion = DateTime.Today;
+            nuevaInscripcion.FechaExpiracion = fechaExpiracion;
+                
+            //Los argumentos los obtengo previemente en el evento aceptar:
+            //idUsuario = int.Parse( ddlCurso.SelectedValue )
+            //idCurso = int.Parse( ddlCurso.SelectedValue )
+            //fechaExpiracion=> calendario = nuevaInscripcion.CursoID;
 
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-
+            datos.AltaInscripcion(nuevaInscripcion);
 
         }
 
         public Inscripcion ObtenerInscripcion(int idUsuario, int idCurso)
         {
-            try
-            {
-                //Validaciones a idUsuario e idCurso
+            InscripcionDatos datos = new InscripcionDatos();
+            Inscripcion seleccionada = new Inscripcion();
 
-                return datos.BuscarInscripcion(idUsuario,idCurso);
-            }
-            catch (Exception ex)
+            //Validaciones a idUsuario e idCurso
+
+            seleccionada = datos.BuscarInscripcion(idUsuario, idCurso);
+
+            if ( seleccionada is null)
             {
-                throw new Exception("No se pudo buscar la Inscripcion.", ex);
+                throw new Exception("No se encontró ninguna inscripcion para Usuario y Curso solicitados.");
             }
+
+            return seleccionada; 
+        
         }
 
         public List<Inscripcion> listarPorUsuario(int idUsuario)
         {
+            InscripcionDatos datos = new InscripcionDatos();
             List<Inscripcion> lista = new List<Inscripcion>();
 
             lista = datos.ListarUsuariosInscripcion(idUsuario);

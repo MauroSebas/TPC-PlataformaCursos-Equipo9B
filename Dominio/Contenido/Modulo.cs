83a7fec19ce754f2bb8591d1f68f397952c0a00c
SebastianDte <sebateunmate@outlook.com>
2025-11-22 20:07:22 -0300

    Feat(Backend): Implementar lógica ABM y reordenamiento automático para Módulos

--- Dominio/Contenido/Modulo.cs
+++ Dominio/Contenido/Modulo.cs
@@ -9,11 +9,18 @@ namespace Dominio
 {
     public class Modulo
     {
-        public int Id { get; set; }
+        public int Id { get; set; }        
+        public int IdCurso { get; set; }        
         public Curso Curso { get; set; }
-        public List<Leccion> Lecciones { get; set; }
+
         public string Nombre { get; set; }
         public int Orden { get; set; }
-        public bool EstaActivo { get; set; }
+        public bool EstaActivo { get; set; } 
+        public List<Leccion> Lecciones { get; set; }        
+        public Modulo()
+        {
+            Lecciones = new List<Leccion>();
+        }
     }
 }
+

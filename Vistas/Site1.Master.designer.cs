c3f81dabcd1ebb304821ad78bf50dbb339f32140
SebastianDte <sebateunmate@outlook.com>
2025-11-25 20:04:43 -0300

    Carrito de compra terminado

--- Vistas/Site1.Master.designer.cs
+++ Vistas/Site1.Master.designer.cs
@@ -51,6 +51,15 @@ namespace Vistas
         protected global::System.Web.UI.WebControls.PlaceHolder phCart;
 
         /// <summary>
+        /// Control litCantCarrito.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal litCantCarrito;
+
+        /// <summary>
         /// Control phBotonAdmin.
         /// </summary>
         /// <remarks>
